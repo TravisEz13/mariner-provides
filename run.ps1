413e2b5588508d8f13ee1a30ab661365a9bf2f50
Travis Plunk <github@ez13.net>
2024-03-15 19:57:25 +0000

    if running on mariner, don't use docker

--- run.ps1
+++ run.ps1
@@ -4,10 +4,18 @@ param(
 )
 
 $extraParams = @()
-if([System.Runtime.InteropServices.RuntimeInformation]::ProcessArchitecture -eq 'Arm64') {
-    $extraParams += @('--build-arg', 'tag=mariner-2.0-arm64')
+
+if (test-path /etc/os-release) {
+    $LinuxInfo = Get-Content /etc/os-release -Raw | ConvertFrom-StringData
+    if ($LinuxInfo.Id -eq 'mariner') {
+        dnf provides $Query
+        return
+    }
 }
 
+if ([System.Runtime.InteropServices.RuntimeInformation]::ProcessArchitecture -eq 'Arm64') {
+    $extraParams += @('--build-arg', 'tag=mariner-2.0-arm64')
+}
 Write-Verbose "running: docker build -t mariner-provides $psscriptroot $extraParams" -Verbose
 docker build -t mariner-provides $psscriptroot $extraParams
 docker run -it --rm mariner-provides $Query
